9c2b33bb5b2e0f9941315e6c3b854c4a557736b3
joydeept <joydeept@microsoft.com>
2014-12-04 12:10:48 -0800

    Second attempt - with more efficient file reading.
    Signed-off-by: joydeept <joydeept@microsoft.com>

--- plot1.R
+++ plot1.R
@@ -3,20 +3,49 @@
 
 plot1 <-
 {
+   
  
-  ### Read in data file
-  ###
-  ### Can't really find a good way to read in just the required rows, so reading in the whole data set for subsetting.
-  
-  d <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", as.is = c(1,2))
-  ### Using as.is to read first two columns as characters instead of factor
-  
+  ######## Generic but inefficient data file reading
+  #
+  ### I could not find a good generic way of reading in just the required rows instead of entire data set.
+  ### Reading the entire file using read.table takes about 30 seconds on my PC.
+  #
+  #d <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", as.is = c(1,2))
+  ### Using as.is to read first two Date and Time columns as characters instead of factor 
+  ### 30 second read time.
   ### Subset only the rows containing dates 2007-02-01 and 2007-02-02
   ### 1st column is Date: Date in format dd/mm/yyyy 
+  #d <- d[grep("^1/2/2007|^2/2/2007", d[,1]),]
+  #
+  ########
   
-  d <- d[grep("^1/2/2007|^2/2/2007", d[,1]),]
   
-  ### Convert Date and Time columns
+  ######## Better file reading, but assumes Windows, Mac or Unix OS.
+  ###
+  ### 
+  ### The fread() function in data.table package allows pre-processing a file using system commands.
+  ### I am assuming the code will be run on either a Windows (findstr), Mac (grep) or Unix (grep) platform. 
+  ### Using fread() reduced read time to 10 seconds.
+  ###
+  ### 
+  
+  library(data.table)
+  
+  ### Detect operating system
+  if (Sys.info()[1] == "Windows") 
+    os_command <- "findstr /b \"[12]/2/2007\" household_power_consumption.txt"
+  else
+    os_command <- "grep \"^[12]/2/2007\" household_power_consumption.txt"
+  
+  ### Selectively read only requried lines.
+  d <- fread(os_command, 
+             sep=";", 
+             na.strings="?",
+             colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+             )
+  ### Header - read just the first line to get column names 
+  h <- read.table("household_power_consumption.txt", nrow=1, header=TRUE, sep=";")
+  setnames(d, names(h))
   
   
   ### Open PNG device
--- plot4.R
+++ plot4.R
@@ -4,21 +4,52 @@
 plot4 <-
 {
  
-  ### Read in data file
-  ###
-  ### Can't really find a good way to read in just the required rows, so reading in the whole data set for subsetting.
-  
-  d <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", as.is = c(1,2))
+  ######## Generic but inefficient data file reading
+  #
+  ### I could not find a good generic way of reading in just the required rows instead of entire data set.
+  ### Reading the entire file using read.table takes about 30 seconds on my PC.
+  #
+  #d <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", as.is = c(1,2))
   ### Using as.is to read first two Date and Time columns as characters instead of factor 
-  
+  ### 30 second read time.
   ### Subset only the rows containing dates 2007-02-01 and 2007-02-02
   ### 1st column is Date: Date in format dd/mm/yyyy 
+  #d <- d[grep("^1/2/2007|^2/2/2007", d[,1]),]
+  #
+  ########
   
-  d <- d[grep("^1/2/2007|^2/2/2007", d[,1]),]
+  
+  ######## Better file reading, but assumes Windows, Mac or Unix OS.
+  ###
+  ### 
+  ### The fread() function in data.table package allows pre-processing a file using system commands.
+  ### I am assuming the code will be run on either a Windows (findstr), Mac (grep) or Unix (grep) platform. 
+  ### Using fread() reduced read time to 10 seconds.
+  ###
+  ### 
+  
+  library(data.table)
+  
+  ### Detect operating system
+  if (Sys.info()[1] == "Windows") 
+    os_command <- "findstr /b \"[12]/2/2007\" household_power_consumption.txt"
+  else
+    os_command <- "grep \"^[12]/2/2007\" household_power_consumption.txt"
+  
+  ### Selectively read only requried lines.
+  d <- fread(os_command, 
+             sep=";", 
+             na.strings="?",
+             colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"),
+             data.table = FALSE)
+  ### Header - read just the first line to get column names 
+  h <- read.table("household_power_consumption.txt", nrow=1, header=TRUE, sep=";")
+  setnames(d, names(h))
   
   ### Concatenate Date and Time columns, convert to POSIXct and form a new data frame
-  col_dt <- strptime(paste(d$Date, d$Time), "%d/%m/%Y %H:%M:%S")
-  d <- cbind(col_dt, d[,3:9])
+  dt <- data.frame(strptime(paste(d$Date, d$Time), "%d/%m/%Y %H:%M:%S"))
+  names(dt) <- c("DateTime")
+  d <- cbind(dt, d[,3:9])
   
   ### Open PNG device
   png(filename="plot4.png", width = 480, height = 480)
@@ -29,16 +60,16 @@ plot4 <-
   with(d, {
     
       ### Plot "Global Active Power"
-      plot(col_dt, Global_active_power, type="l", xlab="", ylab="Global Active Power" )
+      plot(DateTime, Global_active_power, type="l", xlab="", ylab="Global Active Power" )
        
       ### Plot "Voltage"
-      plot(col_dt, Voltage, type="l", xlab="datetime", ylab="Voltage" )
+      plot(DateTime, Voltage, type="l", xlab="datetime", ylab="Voltage" )
        
       
       ### Plot Energy Sub Metering 
-      plot(col_dt, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering", col="black" )
-      lines(col_dt, Sub_metering_2, col="red" )
-      lines(col_dt, Sub_metering_3, col="blue" )
+      plot(DateTime, Sub_metering_1, type="l", xlab="", ylab="Energy sub metering", col="black" )
+      lines(DateTime, Sub_metering_2, col="red" )
+      lines(DateTime, Sub_metering_3, col="blue" )
       legend("topright", 
              legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
              col = (c("black", "red", "blue")), 
@@ -47,7 +78,7 @@ plot4 <-
       
       
       ### Plot "Global Reactive Power"
-      plot(col_dt, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power" )
+      plot(DateTime, Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power" )
       
       
       })
